b3427b81a0babe4087e3ac3e6eea92b64c230dcc
MD Pabel <mdpabel385@gmail.com>
2022-10-22 03:05:01 +0600

    deep clone

---
+++ 01. JS Questions/deep clone.js
@@ -0,0 +1,55 @@
+/**
+ *
+ * JSON.stringify also has few surprising behaviors such as :
+ * converting Date objects to ISO timestamp strings,
+ * NaN and Infinity becoming null etc.
+ */
+const obj = {
+  user: {
+    role: 'admin',
+  },
+  isNull: null,
+  isBool: true,
+  isNum: 1,
+  isStr: 'str',
+  date: new Date(),
+  isNan: NaN,
+  isInfinity: Infinity,
+  isUndefined: undefined,
+};
+
+const copy = JSON.parse(JSON.stringify(obj));
+
+// const copy = obj
+
+console.log(obj === copy);
+console.log(obj, copy);
+
+/**
+ *
+ */
+
+function deepClone(obj) {
+  if (typeof obj !== 'object' || obj === null) return obj;
+  if (Array.isArray(obj)) {
+    return obj.map((val) => deepClone(val));
+  }
+
+  return Object.fromEntries(
+    Object.entries(value).map(([key, val]) => [key, deepClone(val)])
+  );
+}
+
+const copy2 = deepClone(obj);
+
+console.log(obj === copy);
+console.log(obj, copy);
+
+/**
+ * using structuredClone method
+ */
+
+const copy3 = structuredClone(obj);
+
+console.log(obj === copy);
+console.log(obj, copy);
